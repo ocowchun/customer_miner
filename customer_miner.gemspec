7689cad0d6155753c72b836bccdca5056ba944c8
ocowchun <ocowchun@gmail.com>
2017-05-25 16:35:58 +0800

    query clearbit

--- customer_miner.gemspec
+++ customer_miner.gemspec
@@ -17,4 +17,5 @@ Gem::Specification.new do |s|
   s.license       = 'MIT'
 
   s.add_dependency('thor',["~> 0.19.4"])
+  s.add_dependency('httparty',["~> 0.15.5"])
 end
--- lib/customer_miner/query.rb
+++ lib/customer_miner/query.rb
@@ -20,5 +20,21 @@ module CustomerMiner
       rows = CSV.read(@file, headers: true, skip_lines: reg)
       rows.map { |row| row['Clearbit Company Domain'] }.compact.uniq
     end
+
+    def query_clearbit(domain)
+      options = {
+        query: {
+          domain: domain,
+          role: 'marketing'
+        },
+        basic_auth: {
+          username: @secret_key,
+          password: ''
+        }
+      }
+      url = 'https://prospector.clearbit.com/v1/people/search'
+      res = HTTParty.get(url, options)
+      JSON.parse(res)
+    end
   end
 end
